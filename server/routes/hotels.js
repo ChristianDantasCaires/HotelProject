4acdd444068afa292a7ca18c1f9fc32201b6656f
Chr04 <christiandc99@gmail.com>
2022-06-26 14:59:52 -0300

    Crud operations

--- server/routes/hotels.js
+++ server/routes/hotels.js
@@ -19,7 +19,11 @@ router.post("/", async (req, res) => {
 //update
 router.put("/:id", async (req, res) => {
     try {
-        const updatedHotel = await Hotel.findByIdAndUpdate(req.params.id, { $set: req.body });
+        const updatedHotel = await Hotel.findByIdAndUpdate(
+            req.params.id,
+            { $set: req.body },
+            { new: true }
+        )
 
         return res.status(200).json(updatedHotel);
 
@@ -29,7 +33,43 @@ router.put("/:id", async (req, res) => {
 })
 
 //delete
+router.delete("/:id", async (req, res) => {
+    try {
+        await Hotel.findByIdAndDelete(req.params.id);
+
+        return res.status(200).json("Hotel has been deleted.");
+
+    } catch (error) {
+        return res.status(500).json(error);
+    }
+})
+
 //get
+router.get("/:id", async (req, res) => {
+    try {
+        const hotel = await Hotel.findById(req.params.id);
+
+        if (!hotel) {
+            return res.status(400).json({ error: `Internal server error.` });
+        }
+
+        return res.status(200).json(hotel);
+
+    } catch (error) {
+        return res.status(500).json(error);
+    }
+})
+
 //getAll
+router.get("/", async (req, res) => {
+    try {
+        const hotels = await Hotel.find();
+
+        return res.status(200).json(hotels);
+
+    } catch (error) {
+        return res.status(500).json(error);
+    }
+})
 
 export default router;
